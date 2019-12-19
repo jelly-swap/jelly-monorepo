7ed33fde4e6cd6285db98cd68c389f8bf9af9f97
kraykov <raykov@weichain.tech>
2019-12-19 15:11:05 +0200

    Create proper types for ERC20 implementation.

--- packages/erc20/src/types.ts
+++ packages/erc20/src/types.ts
@@ -1,14 +1,27 @@
-export {
-    ContractSwap,
-    ContractWithdraw,
-    ContractRefund,
-    SwapEvent,
-    WithdrawEvent,
-    RefundEvent,
-    JellyContract,
-    UserInputSwap,
-    JellyAdapter,
-} from '@jelly-swap/types';
+import { ContractSwap, ContractRefund, ContractWithdraw, JellyContract, UserInputSwap } from '@jelly-swap/types';
+
+export interface Erc20JellyContract extends Omit<JellyContract, 'getBalance' | 'newContract'> {
+    getBalance(address: string, token: string): Promise<string | number>;
+    newContract(swap: ContractSwap, checkAllowance: boolean): Promise<string>;
+}
+
+export interface Erc20ContractSwap extends ContractSwap {
+    tokenAddress: string;
+}
+
+export interface Erc20ContractRefund extends ContractRefund {
+    tokenAddress: string;
+}
+
+export interface Erc20ContractWithdraw extends ContractWithdraw {
+    tokenAddress: string;
+}
+
+export interface Erc20UserInputSwap extends UserInputSwap {
+    tokenAddress: string;
+}
+
+export { JellyAdapter } from '@jelly-swap/types';
 
 export type Token = {
     network: string;
