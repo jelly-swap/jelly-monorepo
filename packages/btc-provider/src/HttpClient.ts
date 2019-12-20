a4eff66f96cc23b2f60027d8968b18849e8548e1
kraykov <raykov@weichain.tech>
2019-12-20 12:19:39 +0200

    Initial implementation.

---
+++ packages/btc-provider/src/HttpClient.ts
@@ -0,0 +1,39 @@
+import Axios, { AxiosInstance } from 'axios';
+
+export default class BitcoinProvider {
+    private axios: AxiosInstance;
+
+    constructor(baseURL: string) {
+        this.axios = Axios.create({
+            baseURL,
+            responseType: 'text',
+            transformResponse: undefined, // https://github.com/axios/axios/issues/907,
+        });
+    }
+
+    async get(url: string) {
+        try {
+            const response = await this.axios.get(url);
+            return response.data;
+        } catch (err) {
+            if (err.response) {
+                return err.response.data;
+            } else {
+                return err;
+            }
+        }
+    }
+
+    async post(url: string, data: any) {
+        try {
+            const response = await this.axios.post(url, data);
+            return response.data;
+        } catch (err) {
+            if (err.response) {
+                return err.response.data;
+            } else {
+                return err;
+            }
+        }
+    }
+}
