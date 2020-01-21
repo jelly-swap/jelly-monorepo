98998b9def5d9250eb0e28e1662b73d0bcdc6b1d
kraykov <raykov@weichain.tech>
2020-01-21 14:48:19 +0200

    Update Sophia contract.

--- packages/aeternity/src/config/contractSource.ts
+++ packages/aeternity/src/config/contractSource.ts
@@ -1,16 +1,16 @@
 export default `include "List.aes"
-payable contract HashTimeLock = 
 
-  record state = { contracts : map(bytes(32), lock_contract) }
+contract HashTimeLock = 
+
+  record state = { contracts : map(hash, lock_contract) }
 
   datatype status = INVALID | ACTIVE | REFUNDED | WITHDRAWN | EXPIRED 
 
   record lock_contract = {
-    id: bytes(32),
     input_amount : int,
     output_amount : int,
     expiration : int,
-    hash_lock : bytes(32),
+    hash_lock : hash,
     status: status,
     sender : address,
     receiver : address,
@@ -18,29 +18,35 @@ payable contract HashTimeLock =
     output_address : string}
 
   datatype event =
-    Withdraw(indexed bytes(32), indexed address, indexed address, string)
-    | Refund(indexed bytes(32), indexed address, indexed address, string)
-    | New_contract(indexed bytes(32), indexed address, indexed address, string)
+    Withdraw(indexed hash, indexed address, indexed address, string)
+    | Refund(indexed hash, indexed address, indexed address, string)
+    | New_contract(indexed hash, indexed address, indexed address, string)
 
-  function withdrawable(temp_contract : lock_contract, secret : bytes(32)) =
-    require(is_active(temp_contract.status), "SWAP_NOT_ACTIVE")
-    require(temp_contract.expiration > Chain.timestamp, "INVALID_TIME")
-    require(temp_contract.hash_lock == Crypto.sha256(secret), "INVALID_SECRET")
+  function withdrawable(_contract : lock_contract, secret : hash) =
+    require(is_active(_contract.status), "SWAP_NOT_ACTIVE")
+    require(_contract.expiration > Chain.timestamp, "INVALID_TIME")
+    require(_contract.hash_lock == Crypto.sha256(secret), "INVALID_SECRET")
 
-  function refundable(temp_contract: lock_contract) =
-    require(is_active(temp_contract.status), "SWAP_NOT_ACTIVE")
-    require(Chain.timestamp >= temp_contract.expiration, "INVALID_TIME")
-    require(temp_contract.sender == Call.caller, "INVALID_SENDER")
+  function refundable(_contract: lock_contract) =
+    require(is_active(_contract.status), "SWAP_NOT_ACTIVE")
+    require(Chain.timestamp >= _contract.expiration, "INVALID_TIME")
+    require(_contract.sender == Call.caller, "INVALID_SENDER")
 
-  public payable stateful entrypoint new_contract(output_amount : int,
-    expiration : int, hash_lock : bytes(32),
-    receiver : address, output_network : string, output_address : string) : lock_contract =
+  payable stateful entrypoint new_contract(
+      output_amount : int,
+      expiration : int, 
+      hash_lock : hash, 
+      receiver : address, 
+      output_network : string, 
+      output_address : string) : lock_contract =
+    
     let sender : address = Call.caller
+    
     let input_amount : int = Call.value
-    let id : bytes(32) = generate_id(sender, receiver, input_amount, hash_lock, expiration)
-
+    
+    let id : hash = generate_id(sender, receiver, input_amount, hash_lock, expiration)
+    
     let new_contract : lock_contract = { 
-      id = id,
       input_amount = input_amount,
       output_amount = output_amount,
       expiration = expiration,
@@ -62,70 +68,58 @@ payable contract HashTimeLock =
     
     state.contracts[id]
 
-  public stateful entrypoint withdraw(id : bytes(32), secret : bytes(32)) : bool =
-    let temp_contract: lock_contract = state.contracts[id]
+  stateful entrypoint withdraw(id : hash, secret : hash) =
+    let _contract: lock_contract = state.contracts[id]
+    withdrawable(_contract, secret)
 
-    withdrawable(temp_contract, secret)
+    Chain.spend(_contract.receiver, _contract.input_amount)
 
     put(state{contracts[id].status = WITHDRAWN})
 
-    Chain.spend(temp_contract.receiver, temp_contract.input_amount)
+    Chain.event(Withdraw(id, _contract.sender, _contract.receiver, generate_withdraw_message(secret, _contract.hash_lock) ))
 
-    let withdraw_message : string = generate_withdraw_message(secret, temp_contract.hash_lock)
-
-    Chain.event(Withdraw(id, temp_contract.sender, temp_contract.receiver, withdraw_message ))
-
-    true
-
-  public stateful entrypoint refund(id : bytes(32)) : bool =
-    let temp_contract : lock_contract = state.contracts[id]
-
-    refundable(temp_contract)
+  stateful entrypoint refund(id : hash) =
+    let _contract : lock_contract = state.contracts[id]
+    refundable(_contract)
+    
+    Chain.spend(_contract.sender, _contract.input_amount)
 
     put(state{ contracts[id].status = REFUNDED })
-    
-    Chain.spend(temp_contract.sender, temp_contract.input_amount)
-    
-    Chain.event(Refund(id, temp_contract.sender, temp_contract.receiver, Bytes.to_str(temp_contract.hash_lock)))
-    
-    true
 
-  public entrypoint get_one_status(id : bytes(32)) : status =
-    let temp_contract : lock_contract = state.contracts[id]
-    if(temp_contract.status == ACTIVE &&
-        temp_contract.expiration < Chain.timestamp)
+    Chain.event(Refund(id, _contract.sender, _contract.receiver, Bytes.to_str(_contract.hash_lock)))
+
+  entrypoint get_one_status(id : hash) : status =
+    let _contract : lock_contract = state.contracts[id]
+
+    if(_contract.status == ACTIVE &&
+        _contract.expiration < Chain.timestamp)
       EXPIRED
     else
-      temp_contract.status
+      _contract.status
 
-  public entrypoint get_many_status(ids : list(bytes(32))) : list(status) =
+  entrypoint get_many_status(ids : list(hash)) : list(status) =
     List.map((id) => get_one_status(id), ids)
 
-  entrypoint generate_id(sender : address, receiver : address,
-   input_amount : int, hash_lock : bytes(32), expiration : int) : bytes(32) =
+  function generate_id(sender : address, receiver : address,
+   input_amount : int, hash_lock : hash, expiration : int) : hash =
     let packed_string : string = String.concat(String.concat(String.concat(Address.to_str(sender), 
       Address.to_str(receiver)), String.concat(Int.to_str(input_amount),
        Bytes.to_str(hash_lock))), Int.to_str(expiration))
-
     Crypto.sha256(packed_string)
+    
+  function generate_withdraw_message(secret : hash, hash_lock : hash) : string =
+    concat(Bytes.to_str(secret), Bytes.to_str(hash_lock))
 
-  entrypoint generate_withdraw_message(secret : bytes(32), hash_lock : bytes(32)) : string =
-    let withdraw_message : string = concat(Bytes.to_str(secret), Bytes.to_str(hash_lock))
-    withdraw_message
-
-  entrypoint generate_swap_message(new_contract : lock_contract) : string =
-    let swap_message : string = concat(concat(new_contract.output_network, new_contract.output_address),
+  function generate_swap_message(new_contract : lock_contract) : string =
+    concat(concat(new_contract.output_network, new_contract.output_address),
      concat(concat(Int.to_str(new_contract.input_amount), Int.to_str(new_contract.output_amount)),
      concat(Int.to_str(new_contract.expiration), Bytes.to_str(new_contract.hash_lock))))
-    swap_message
   
-  entrypoint is_active(x : status) : bool =
-    switch(x)
-      ACTIVE => true
-      _ => false
+  function is_active(x : status) : bool =
+    x == ACTIVE
 
-  entrypoint concat(a : string, b : string) =
+  function concat(a : string, b : string) =
     String.concat(String.concat(a, ","),b)
 
-  public entrypoint  init() = { contracts = {} }
+  entrypoint  init() = { contracts = {} }
 `;
