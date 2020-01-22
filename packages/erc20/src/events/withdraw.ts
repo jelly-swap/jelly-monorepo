fa23d497d22c1192a70088d1f506d707490b1a30
kraykov <raykov@weichain.tech>
2020-01-22 12:05:15 +0200

    Allow passing ERC20 configuration.

--- packages/erc20/src/events/withdraw.ts
+++ packages/erc20/src/events/withdraw.ts
@@ -1,9 +1,7 @@
-import { AddressToToken } from '../config/tokens';
-
-export default (args: any[], txHash?: any) => {
+export default (args: any[], config: any, txHash?: any) => {
     const transactionHash = args[6] ? args[6].transactionHash : txHash;
     const tokenAddress = args[3].toLowerCase();
-    const token = AddressToToken[tokenAddress].network;
+    const token = config.AddressToToken[tokenAddress].network;
 
     if (token) {
         const result = {
