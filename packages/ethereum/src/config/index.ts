4500d1bbc9f1176e95f0858d2c4459d08f254767
kraykov <raykov@weichain.tech>
2020-03-08 23:29:16 +0200

    Add originBlock - the contract creation block.

--- packages/ethereum/src/config/index.ts
+++ packages/ethereum/src/config/index.ts
@@ -4,6 +4,7 @@ export default (expiration = 10800) => {
         explorer: 'https://ropsten.etherscan.io/tx/',
         providerUrl: 'https://ropsten.infura.io/v3/8fe4fc9626494d238879981936dbf144',
         contractAddress: '0xc961b206901a821affddd77fac831e9ff1be89c0',
+        originBlock: 6812604,
         receiverAddress: '0xF684C21Ec023E93da0B402ac0a274317eb51C2c7',
         blockTime: 15,
         expiration,
