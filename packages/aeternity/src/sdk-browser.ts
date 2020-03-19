6721d6d197250c56a6849c08dae15e583a6968a4
kraykov <raykov@weichain.tech>
2020-03-19 12:45:11 +0200

    Import the sdk in a single file for both browser and nodejs.

---
+++ packages/aeternity/src/sdk-browser.ts
@@ -0,0 +1,5 @@
+import { decodeBase64Check, addressFromDecimal } from '@aeternity/aepp-sdk/es/utils/crypto';
+
+export { Node, Universal, MemoryAccount } from '@aeternity/aepp-sdk/es';
+
+export { decodeBase64Check, addressFromDecimal };
---
+++ packages/aeternity/src/sdk-node.ts
@@ -0,0 +1,6 @@
+import { Crypto } from '@aeternity/aepp-sdk';
+
+export { Node, Universal, MemoryAccount } from '@aeternity/aepp-sdk';
+
+export const decodeBase64Check = Crypto.decodeBase64Check;
+export const addressFromDecimal = Crypto.addressFromDecimal;
