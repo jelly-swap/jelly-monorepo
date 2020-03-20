3b028a85f94ebd2378b821b3cc5e4f0b2299bc54
kraykov <raykov@weichain.tech>
2020-03-20 18:33:16 +0200

    Add utils and entry file.

---
+++ packages/aex9/src/index.ts
@@ -0,0 +1,9 @@
+import { Providers } from '@jelly-swap/aeternity';
+
+import Contract from './contract';
+import Config from './config';
+import Adapter from './adapter';
+import Event from './events';
+import * as Types from './types';
+
+export { Contract, Config, Adapter, Event, Providers, Types };
