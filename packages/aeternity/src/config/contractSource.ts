6c996dd1c3c4e09492606e4604b63587aa7d74dc
kraykov <raykov@weichain.tech>
2020-03-26 14:19:10 +0200

    Update contract interface.

--- packages/aeternity/src/config/contractSource.ts
+++ packages/aeternity/src/config/contractSource.ts
@@ -1,19 +1,34 @@
 export default `@compiler >= 4
-
 include "List.aes"
 
-contract HashTimeLock = 
+contract JellyHTLC = 
+
+  record state = { swaps : map(hash, swap) }
+
   datatype status = INVALID | ACTIVE | REFUNDED | WITHDRAWN | EXPIRED 
 
-  datatype event =
-    Withdraw(indexed hash, indexed address, indexed address, string)
-    | Refund(indexed hash, indexed address, indexed address, string)
-    | New_contract(indexed hash, indexed address, indexed address, string)
+  record swap = {
+    input_amount : int,
+    output_amount : int,
+    expiration : int,
+    hash_lock : hash,
+    status: status,
+    sender : address,
+    receiver : address,
+    output_network : string,
+    output_address : string}
 
-  stateful payable entrypoint new_contract  : (int, int, hash, address, string, string) => unit
-  stateful entrypoint withdraw              : (hash, hash) => unit
-  stateful entrypoint refund                : (hash) => unit
-  entrypoint get_one_status                 : (hash) => status
-  entrypoint get_many_status                : (list(hash)) => list(status)
-  entrypoint generate_id                    : (address, address, int, hash, int) => hash
+  datatype event =
+    Withdraw(hash, address, address, string)
+    | Refund(hash, address, address, string)
+    | NewSwap(hash, address, address, string)
+
+  stateful payable entrypoint new_swap : (int, int, hash, address, string, string) => unit
+  stateful entrypoint withdraw         : (hash, hash)                              => unit
+  stateful entrypoint refund           : (hash)                                    => unit
+  
+  entrypoint get_one_status            : (hash)                                    => status
+  entrypoint get_many_status           : (list(hash))                              => list(status)
+  entrypoint generate_id               : (address, address, int, hash, int)        => hash
+  entrypoint get_swap                  : (hash)                                    => swap
 `;
--- packages/aeternity/src/config/index.ts
+++ packages/aeternity/src/config/index.ts
@@ -6,7 +6,7 @@ export default (expiration = 10800) => {
         internalUrl: 'https://testnet.aeternity.io/',
         compilerUrl: 'https://latest.compiler.aepps.com',
         wsUrl: 'wss://testnet.aeternal.io/websocket',
-        contractAddress: 'ct_2f7rhRtba2G3SG6yrQoerJsT53XDbv9iNqY1m1MmxWWnuwbjvh',
+        contractAddress: 'ct_27JLKSzWS1ZV8Kzu6FyDpmC16xAAAJqwPtVv3dECmswR2zdeFN',
         receiverAddress: 'ak_471dYUrQ8EAtmzwuKDw4VBGQdnEcP5YF563WG4yR9Wvfp5tRp',
         blockTime: 15,
         expiration,
