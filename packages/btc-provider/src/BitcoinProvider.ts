527d96f6c0d3ea3e5c81ae3aaeb8dcb1293356a3
kraykov <raykov@weichain.tech>
2020-04-14 17:41:46 +0300

    Add cache. New interface - getBalance, getCurrentBlock

--- packages/btc-provider/src/BitcoinProvider.ts
+++ packages/btc-provider/src/BitcoinProvider.ts
@@ -1,15 +1,31 @@
 import BigNumber from 'bignumber.js';
+import memoize from 'memoizee';
 
 import HttpClient from './HttpClient';
 
+import CacheConfig from './CacheConfig';
+
 export default class BitcoinProvider {
     private httpClient: HttpClient;
+    private getUnspentTransactionsCache: Function;
+    private getCurrentBlockCache: Function;
 
     constructor(baseURL: string) {
         this.httpClient = new HttpClient(baseURL);
+
+        this.getUnspentTransactionsCache = memoize(this._getUnspentTransactions, CacheConfig());
+        this.getCurrentBlockCache = memoize(this._getCurrentBlock, CacheConfig());
     }
 
-    async getBlockHeight() {
+    async getCurrentBlock(cache = false) {
+        if (cache) {
+            return await this.getCurrentBlockCache();
+        }
+
+        return await this._getCurrentBlock();
+    }
+
+    async _getCurrentBlock() {
         const response = await this.httpClient.get(`/block/tip`);
         return response.height;
     }
@@ -27,9 +43,29 @@ export default class BitcoinProvider {
         return result;
     }
 
+    async getUnspentTransactions(addresses: any, cache = false) {
+        if (cache) {
+            return await this.getUnspentTransactionsCache(addresses);
+        }
+
+        return await this._getUnspentTransactions(addresses);
+    }
+
+    async getBalance(addresses: any) {
+        const utxos = await this.getUnspentTransactions(addresses, true);
+
+        const balance = utxos
+            .reduce((prev: BigNumber, curr: any) => {
+                return prev.plus(new BigNumber(curr.value));
+            }, new BigNumber(0))
+            .toNumber();
+
+        return balance;
+    }
+
     // TODO: find a way to improve the logic
-    async getUnspentTransactions(addresses: any) {
-        const currentHeight = await this.getBlockHeight();
+    async _getUnspentTransactions(addresses: any) {
+        const currentHeight = await this.getCurrentBlock();
 
         const queries = [];
         let addressQuery = '';
