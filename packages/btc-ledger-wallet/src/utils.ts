c3bd9711a811d3379a086f96360801f5ff6b674e
kraykov <kraykov1994@gmail.com>
2020-04-23 12:15:01 +0300

    padHexStart and getAmountBuffer moved to utils.

--- packages/btc-ledger-wallet/src/utils.ts
+++ packages/btc-ledger-wallet/src/utils.ts
@@ -1,3 +1,5 @@
+import { BigNumber } from 'bignumber.js';
+
 const createVarint = (value: number): Buffer => {
     if (value < 0xfd) {
         const buffer = Buffer.alloc(1);
@@ -19,6 +21,7 @@ const createVarint = (value: number): Buffer => {
     buffer[4] = (value >> 24) & 0xff;
     return buffer;
 };
+
 export const serializeTransactionOutputs = (outputs: any): Buffer => {
     let outputBuffer = Buffer.alloc(0);
     if (typeof outputs !== 'undefined') {
@@ -33,4 +36,19 @@ export const serializeTransactionOutputs = (outputs: any): Buffer => {
         });
     }
     return outputBuffer;
+};
+
+export const padHexStart = (hex: string, length: number) => {
+    let len = length || hex.length;
+    len += len % 2;
+
+    return hex.padStart(len, '0');
+};
+
+export const getAmountBuffer = (amount: number) => {
+    let hexAmount = new BigNumber(Math.round(amount)).toString(16);
+
+    hexAmount = padHexStart(hexAmount, 16);
+    const valueBuffer = Buffer.from(hexAmount, 'hex');
+    return valueBuffer.reverse();
 };
