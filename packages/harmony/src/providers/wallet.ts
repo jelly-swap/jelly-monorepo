0ceb552766a5dac15cdf12924d6ab438ef4b91c8
kraikov <raykov@weichain.tech>
2020-08-10 16:08:50 +0300

    Add sendTransaction wrapper.

--- packages/harmony/src/providers/wallet.ts
+++ packages/harmony/src/providers/wallet.ts
@@ -1,15 +1,36 @@
 import { Wallet } from '@harmony-js/account';
 import { Messenger, HttpProvider } from '@harmony-js/network';
+import { TransactionFactory } from '@harmony-js/transaction';
 import { ChainID, ChainType } from '@harmony-js/utils';
 
 import Config from '../config';
 
 export default class WalletProvider extends Wallet {
+    private transactionFactory: TransactionFactory;
+
     constructor(providerUrl = Config().providerUrl, privateKey?: string) {
-        super(new Messenger(new HttpProvider(providerUrl), ChainType.Harmony, ChainID.HmyTestnet));
+        const messenger = new Messenger(new HttpProvider(providerUrl), ChainType.Harmony, ChainID.HmyTestnet);
+        super(messenger);
+
+        this.transactionFactory = new TransactionFactory(messenger);
 
         if (privateKey) {
             this.addByPrivateKey(privateKey);
         }
+    }
+
+    async sendTransaction(receiver: string, value: string) {
+        const txn = this.transactionFactory.newTx({
+            to: receiver,
+            value,
+            gasLimit: '210000',
+            gasPrice: '100000000000',
+        });
+
+        const signedTxn = await this.signTransaction(txn);
+
+        await signedTxn.sendTransaction();
+
+        return signedTxn as any;
     }
 }
