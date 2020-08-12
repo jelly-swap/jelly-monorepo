052a05e3865e0ac0a39125910d0508fb7e329b58
kraikov <raykov@weichain.tech>
2020-08-12 18:02:55 +0300

    Update Ethereum package.

--- packages/ethereum/src/index.ts
+++ packages/ethereum/src/index.ts
@@ -1,7 +1,6 @@
 import Contract from './contract';
 import Config from './config';
 import Adapter from './adapter';
-import Event from './events';
 import Providers from './providers';
 
-export { Contract, Config, Adapter, Event, Providers };
+export { Contract, Config, Adapter, Providers };
